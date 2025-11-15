38032eb52309f1e2afc75aea477c19e9e2b86da3
Rei Yada <ryada@c2r6p8.42perpignan.fr>
2025-11-15 17:27:12 +0100

    mariadb modified

--- inception/srcs/requirements/mariadb/tools/init-mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/init-mariadb.sh
@@ -3,11 +3,9 @@
 # ---------------------------------------------------------------------------- #
 #!/usr/bin/env bash
   #->to tell the OS to run this file with the bash interpreter found via env
-set -euo pipefail
+set -eu
   #->-e: exit immediately if any command fails
   #->-u: treat use of unset variables are an error
-  #->-o pipefail: if you use pipe ( a | b ) and if any command fails, the whole pipeline fails
-
 
 # ---------------------------------------------------------------------------- #
 #                                 Read secrets                                 #
@@ -29,40 +27,26 @@ DB_PASSWORD="$(cat /run/secrets/db_password)"
 #                          Initialize datadir if empty                         #
 # ---------------------------------------------------------------------------- #
   #->if the MariaDB data dir has never been initialized before...
+    #->you need to create DB and users
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-echo "[entrypoint] Initializing database..."
-  install -d -o mysql -g mysql /var/lib/mysql
-  mariadb-install-db --user=mysql --datadir=/var/lib/mysql >/dev/null
+    echo "[entrypoint] Initializing MariaDB..."
 
-  #Temp server to run SQL
-  mysqld --user=mysql --datadir=/var/lib/mysql --skip-networking &
-  pid="$!"
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql >/dev/null
 
-  #Wait for socket
-  for i in {30..0}; do
-    mariadb-admin ping --silent && break
-    sleep 1
-  done
+    #Run SQL initialization using bootstrap mode
+    mariadbd --user=mysql --datadir=/var/lib/mysql --bootstrap <<-EOF
+        FLUSH PRIVILEGES;
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+        CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+        CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+        GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+        FLUSH PRIVILEGES;
+EOF
 
-  if ! mariadb-admin ping --silent; then
-    echo "MariaDB failed to start for initialization" >&2
-    exit 1
-  fi
-
-  #Secure + set up DB/users
-  mariadb <<SQL
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
-CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
-SQL
-
-  mariadb-admin shutdown
-  wait "$pid" || true
-  echo "[entrypoint] Initialization done."
+    echo "[entrypoint] Initialization complete."
 fi
+
+echo "[entrypoint] Starting MariaDB..."
 
 # ---------------------------------------------------------------------------- #
 #              Exec mysqld as PID 1 (no tail -f / infinite loops)              #
